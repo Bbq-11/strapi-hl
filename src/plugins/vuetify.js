70373172b5262f495b32a02d81022462665cde91
Bbq-11 <chechetkin01@mail.ru>
2024-05-10 11:31:52 +0300

    addуed sections

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,19 +1,16 @@
 import { createVuetify } from 'vuetify';
 import { aliases, mdi } from 'vuetify/iconsets/mdi-svg';
-import colors from 'vuetify/util/colors';
 
 export default createVuetify({
-    aliases: {
-    },
-    defaults: {
-    },
+    aliases: {},
+    defaults: {},
     theme: {
         defaultTheme: 'light',
         themes: {
             light: {
                 dark: false,
-            }
-        }
+            },
+        },
     },
     icons: {
         aliases,
